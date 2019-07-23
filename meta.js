c44eced768ccfdff86dd6f466aa765f59a13b4ff
Martin Semerad <daremes@Martins-MacBook-Pro-2.local>
2019-07-23 09:08:22 +0200

    add docs

---
+++ meta.js
@@ -0,0 +1,19 @@
+module.exports = {
+  sections: [
+    {
+      name: 'Intro',
+      depth: 0,
+      content: 'Intro.mdx',
+      sections: [
+        { name: 'Konvence komitovani', 
+          content: 'Commit-Conventions.mdx',
+          customPath: 'conventions', 
+        },
+        {
+          name: 'Dalsi dokument',
+          content: 'Other-Documents.mdx',
+        },
+      ],
+    },
+  ]
+};
